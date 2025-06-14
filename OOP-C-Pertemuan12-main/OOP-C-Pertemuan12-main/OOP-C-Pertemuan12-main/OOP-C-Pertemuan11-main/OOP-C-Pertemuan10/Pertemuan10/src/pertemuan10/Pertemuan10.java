18a76b5be364a9856eb7715d8f5a1db33687abfa
ardinajandrilia <ardinajandrilia4118@gmail.com>
2025-06-14 14:52:33 +0800

    Add files via upload

---
+++ OOP-C-Pertemuan12-main/OOP-C-Pertemuan12-main/OOP-C-Pertemuan12-main/OOP-C-Pertemuan11-main/OOP-C-Pertemuan10/Pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,18 @@
+
+package pertemuan10;
+
+/**
+ *
+ * @author Rambu Intan
+ * TGL 24 Mei 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ OOP-C-Pertemuan12-main/OOP-C-Pertemuan12-main/OOP-C-Pertemuan12-main/OOP-C-Pertemuan11-main/OOP-C-Pertemuan10/Pertemuan10/src/pertemuan10/dbKoneksi.java
@@ -0,0 +1,31 @@
+
+package pertemuan10;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LAB F
+ */
+public class dbKoneksi {
+    static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static String DB_URL="jdbc:mysql://localhost/mahasiswa";
+    static String DB_USER="root";
+    static String DB_PASS="";
+    
+    static public Connection koneksi(){
+        try{
+            Class.forName(JDBC_DRIVER);
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            
+        } catch(ClassNotFoundException | SQLException err ){
+        System.out.println("Gagal Koneksi ke DBMS MySQL");
+        }
+        
+        return null;
+    }
+}
+    
+
